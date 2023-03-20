01940a90929832215b70254589ecbd5dc7a9ec2c
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-20 12:12:42 -0400

    updated apollo server to include typedefs and resolvers

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+
+const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const routes = require("./routes");
 
@@ -22,6 +24,20 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
