2ef913a355195275890117aa8f8c34c649e8a884
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-21 14:57:51 -0400

    updated apollo server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,7 +23,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
