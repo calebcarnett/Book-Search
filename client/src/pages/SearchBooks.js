2728f6305c07000d0f4ec0fe176926a0060dffaa
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-21 16:02:27 -0400

    fixed login and sign up form to use mutation functionality

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -96,23 +96,23 @@ const SearchBooks = () => {
         <Container>
           <h1>Search for Books!</h1>
           <Form onSubmit={handleFormSubmit}>
-            <Form.Row>
-              <Col xs={12} md={8}>
-                <Form.Control
-                  name="searchInput"
-                  value={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                  type="text"
-                  size="lg"
-                  placeholder="Search for a book"
-                />
-              </Col>
-              <Col xs={12} md={4}>
-                <Button type="submit" variant="success" size="lg">
-                  Submit Search
-                </Button>
-              </Col>
-            </Form.Row>
+            {/* <Form.Row> */}
+            <Col xs={12} md={8}>
+              <Form.Control
+                name="searchInput"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                type="text"
+                size="lg"
+                placeholder="Search for a book"
+              />
+            </Col>
+            <Col xs={12} md={4}>
+              <Button type="submit" variant="success" size="lg">
+                Submit Search
+              </Button>
+            </Col>
+            {/* </Form.Row> */}
           </Form>
         </Container>
       </div>
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,5 @@
+import { gql } from "@apollo/client";
+
 export const GET_ME = gql`
   query users($_id: String) {
     users(_id: $_id) {
